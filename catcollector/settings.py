5ffdebba36ad47f5beee4d37bbf27ba93ec71353
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2021-01-14 12:46:57 -0500

    Add models and cats to database

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secrets import SECRET_DJANGO_KEY, POSTGRESPW
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o+_dx+7*wznd4!w0v*ikb65^mm*))!v40(!6)(t#x3ply5dr01'
+SECRET_KEY = SECRET_DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,7 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cats',
-
+        'USER': 'postgres',
+        'PASSWORD': POSTGRESPW,
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Cat
+
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
+# cats = [
+#     Cat('Lolo', 'tabby', 'foul little demon', 3),
+#     Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#     Cat('Raven', 'black tripod', '3 legged cat', 4)
+# ]
 
 # Create your views here.
 def index(request):
@@ -6,3 +19,7 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cats_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
