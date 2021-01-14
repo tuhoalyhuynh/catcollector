ba042f9c05c124a00af5bb8c632a379023416187
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2021-01-14 15:49:32 -0500

    Add routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Cat
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 def index(request):
@@ -8,10 +10,29 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 
+# Cats
 def cats_index(request):
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
 
 def cats_show(request, cat_id):
-  cat = Cat.objects.get(id=cat_id)
-  return render(request, 'cats/show.html', { 'cat': cat })
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/show.html', { 'cat': cat })
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = '/cats'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        return HttpResponseRedirect('/cats')
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats'
