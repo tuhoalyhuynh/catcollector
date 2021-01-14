7af19a6193ddaccba4b84dba528a468b022a8766
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2021-01-14 14:04:31 -0500

    [project] Debug errors

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,6 @@
 from django.shortcuts import render
 from .models import Cat
 
-# class Cat:
-#     def __init__(self, name, breed, description, age):
-#         self.name = name
-#         self.breed = breed
-#         self.description = description
-#         self.age = age
-# cats = [
-#     Cat('Lolo', 'tabby', 'foul little demon', 3),
-#     Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-#     Cat('Raven', 'black tripod', '3 legged cat', 4)
-# ]
-
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -23,3 +11,7 @@ def about(request):
 def cats_index(request):
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cats_show(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/show.html', { 'cat': cat })
